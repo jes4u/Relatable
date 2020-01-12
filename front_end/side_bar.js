b14a2151d10eb08c5e7ed6ffe4d43f01f6daaac6
Jesse <jes4u@uw.edu>
2020-01-12 04:50:45 -0800

    started redesigning extension

--- front_end/side_bar.js
+++ front_end/side_bar.js
@@ -1,21 +1,26 @@
 var letsgetthisbread = async () => {
-    const url = "http:/127.0.0.1:5000/query/?url=https://hackernoon.com/top-python-web-development-frameworks-to-learn-in-2019-21c646a09a9a";
+    const url = "http:/206.87.108.98:5000/query/?url=" + window.location.href;
     let request = new Request(url, {
         method: "GET",
         headers: new Headers()
     });
     this.callBackend(request)
         .then(res => {
-            var json = res.body;
-            document.getElementById('test').innerHTML = json;
-            var links_output = document.getElementById("links");
-            links_output.innerHTML = '';
-            for (var key of json) {
-                links_output.innerHTML = links_output.innerHTML + '<div><h3>' + key + '</h3><br>';
-                for (var links of json[key]) {
-                    links_output.innerHTML = links_output.innerHTML + '<a href="' + '">' + json[key][links] + '</a></div><br>';
-                }
+            document.getElementById("test").innerHTML = res;
+            var cards = document.getElementsByClassName("card-body");
+            for ( var i = 0 ; i < cards.length ; i++) {
+                cards[i].child().innerHTML = res.pages[i];
             }
+//            var json = res.body;
+//            document.getElementById('test').innerHTML = json;
+//            var links_output = document.getElementById("links");
+//            links_output.innerHTML = '';
+//            for (var key of json) {
+//                links_output.innerHTML = links_output.innerHTML + '<div><h3>' + key + '</h3><br>';
+//                for (var links of json[key]) {
+//                    links_output.innerHTML = links_output.innerHTML + '<a href="' + '">' + json[key][links] + '</a></div><br>';
+//                }
+//            }
         })
         .catch(err => console.log(err));
 };
