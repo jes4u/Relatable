5e9a72e5fd1ab7d6bb07fb1093030a4779587f7c
nfsrun <kevin.tran@bellevuecollege.edu>
2020-01-11 22:32:43 -0800

    First commit lol

---
+++ main.js
@@ -0,0 +1,24 @@
+chrome.runtime.onInstalled.addListener(function() {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([{
+            // That fires when a page's URL contains a 'g' ...
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { urlContains: 'g' },
+                })
+            ],
+            // And shows the extension's page action.
+            actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    });
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+    console.log("Installed boi");
+});
+
+chrome.runtime.onStartup.addListener(function() {
+    console.log("Opened boi")
+});
+
