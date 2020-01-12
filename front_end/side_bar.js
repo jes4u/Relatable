08b44d2c86a81ae0c52d73ea7794c284d18e22cb
Jesse <jes4u@uw.edu>
2020-01-12 11:33:08 -0800

    css stuff

--- front_end/side_bar.js
+++ front_end/side_bar.js
@@ -1,17 +1,23 @@
 // basically still need to figure out how to store current url.
 // ---------
-chrome.tabs.getSelected(null, function(tab) {
-    myFunction(tab.url);
-});
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+   function(tabs){
+      letsgetthisbread(tabs[0].url);
+   }
+);
+
 
 function myFunction(tablink) {
   // do stuff here
   console.log(tablink);
 }
-// ---------
 
-var letsgetthisbread = async () => {
-    const url = "http:/206.87.108.98:5000/query/?url=https://hackernoon.com/top-python-web-development-frameworks-to-learn-in-2019-21c646a09a9a";
+var currentBody = document.getElementsByTagName("BODY")[0];
+console.log(currentBody);
+
+var letsgetthisbread = async (url2) => {
+//    const url = "http:/206.87.108.98:5000/query/?url=https://hackernoon.com/top-python-web-development-frameworks-to-learn-in-2019-21c646a09a9a";
+    const url = "http:/206.87.108.98:5000/query/?url=" + url2;
     let request = new Request(url, {
         method: "GET",
         headers: new Headers()
@@ -19,20 +25,19 @@ var letsgetthisbread = async () => {
     this.callBackend(request)
         .then(res => {
             console.log(res)
+
             var cards = document.getElementsByClassName("card-body");
             for ( var i = 0 ; i < cards.length ; i++) {
                 cards[i].innerHTML = "<a href="+res.pages[i]+" target='_blank'>" + res.titles[i] + "</a>";
             }
-//            var json = res.body;
-//            document.getElementById('test').innerHTML = json;
-//            var links_output = document.getElementById("links");
-//            links_output.innerHTML = '';
-//            for (var key of json) {
-//                links_output.innerHTML = links_output.innerHTML + '<div><h3>' + key + '</h3><br>';
-//                for (var links of json[key]) {
-//                    links_output.innerHTML = links_output.innerHTML + '<a href="' + '">' + json[key][links] + '</a></div><br>';
-//                }
-//            }
+            var afterLoad = document.getElementsByClassName("afterLoad");
+            for ( var i = 0 ; i < afterLoad.length ; i++) {
+                afterLoad[i].classList.remove("afterLoad");
+            }
+            var beforeLoad = document.getElementsByClassName("beforeLoad");
+            for ( var i = 0 ; i < beforeLoad.length ; i++) {
+                beforeLoad[i].classList.add("toggleOff");
+            }
         })
         .catch(err => console.log(err));
 };
