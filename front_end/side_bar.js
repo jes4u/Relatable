5612410238b95e0886182b86e102703dbf79ea1a
nfsrun <kevin.tran@bellevuecollege.edu>
2020-01-12 02:11:34 -0800

    hmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- front_end/side_bar.js
+++ front_end/side_bar.js
@@ -3,17 +3,18 @@ var letsgetthisbread = function() {
         var post = new XMLHttpRequest();
         
         //change this
-        var url_get = "10.19.133.160:5000/query/?url=https://hackernoon.com/top-python-web-development-frameworks-to-learn-in-2019-21c646a09a9a";
+        var url_get = "https://206.87.108.98:5000/query/?url=https://hackernoon.com/top-python-web-development-frameworks-to-learn-in-2019-21c646a09a9a";
         
         post.open("GET", url_get, true);
-
+        document.getElementById('test').innerHTML = document.body.innerHTML;
         //possibly change this
         post.setRequestHeader('Content-Type', 'application/json');
         //post.send(document.body.innerHTML);
+        post.send();
         if (post.status == 200) {
             
             var json = response.body;
-            document.getElementById('test').innerHTML = (json);
+            document.getElementById('test').innerHTML = json;
             var links_output = document.getElementById("links");
             links_output.innerHTML = '';
             for (var key of json) {
@@ -23,13 +24,14 @@ var letsgetthisbread = function() {
                 }
             }
             resolve();
-      } else {
+        } else {
+            document.getElementById('test').innerHTML = 'poopy';
             reject();
       }
     });
   }
 
 window.onload = function() {
-    letsgetthisbread()
+    letsgetthisbread();
 }
 
